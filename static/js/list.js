26510d25ca3fa19179282e6faa7b720a5706a982
= <=>
2021-03-02 15:08:08 +0900

    list html, css, js 추가

---
+++ static/js/list.js
@@ -0,0 +1,29 @@
+function infinity(){
+  window.onscroll = function(e){
+    if((window.innerHeight + window.scrollY) >= document.body.offsetHeight){
+      console.log('다 내려왔다.')
+      const temp_html = `<div class="card" style="width: 350px">
+                            <img src="../static/img.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h5 class="card-title">
+                              <a href="...">첫번째 카드</a> //TODO: 주소 변경 필요
+                              </h5>
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                              <li class="list-group-item">작성일</li>
+                              <li class="list-group-item">작성자 아이디</li>
+                            </ul>
+                        </div>
+`
+      $('.card__wrap').append(temp_html);
+    }
+  }
+}
+
+
+
+
+
+
+infinity();
