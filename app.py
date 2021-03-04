34811f8d23d44ae052e2237f4ca508b5ef9e7bb0
danaisboss <yjk9313@gamil.com>
2021-03-04 09:36:55 +0900

    detail.html > review_detail로 수정

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def show_reviews():
 def detail_reviews(review_id):
     #img = list(db.reviews.find({'reivew_file': img}))
     reviewId = list(db.reviews.find({'_id': review_id}))
-    return render_template('detail.html', reviewId=reviewId)
+    return render_template('review_detail.html', reviewId=reviewId)
 
 
 @app.route('/review_update/<id_data>')
